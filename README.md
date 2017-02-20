b68ad5e75f8476ee9f837bf2e0da7e546eca0a77
zqs.lfwl <823969832@qq.com>
2017-02-20 16:47:42 +0800

    update

---
+++ README.md
@@ -0,0 +1,5 @@
+unioncentera
+Test commit
+Test ssh connect
+===============
+rest 框架
---
+++ unioncenter-core/src/main/java/com/zqs/core/service/impl/DefaultTestServiceImpl.java
@@ -0,0 +1,13 @@
+package com.zqs.core.service.impl;
+
+import com.zqs.core.service.TestService;
+
+public class DefaultTestServiceImpl implements TestService {
+
+	@Override
+	public String hi() {
+		
+		return "hi";
+	}
+
+}
